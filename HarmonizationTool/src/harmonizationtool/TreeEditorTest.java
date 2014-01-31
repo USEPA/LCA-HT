8a0a9ca86cb5c400dd076255e2ef096614e5f3c9
Tom Transue <Tom@Lynn-PC>
2014-01-31 10:53:38 -0500

    - TreeEditorTest can save selected TreeNode for analysis and alteration

--- HarmonizationTool/src/harmonizationtool/TreeEditorTest.java
+++ HarmonizationTool/src/harmonizationtool/TreeEditorTest.java
@@ -39,6 +39,8 @@ import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.jface.viewers.ViewerCell;
 import org.eclipse.jface.viewers.ViewerRow;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.KeyEvent;
+import org.eclipse.swt.events.KeyListener;
 import org.eclipse.swt.events.MouseEvent;
 import org.eclipse.swt.events.MouseListener;
 import org.eclipse.swt.events.SelectionEvent;
@@ -52,49 +54,43 @@ import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Tree;
+import org.eclipse.swt.widgets.TreeColumn;
+import org.eclipse.swt.widgets.TreeItem;
 
 import com.hp.hpl.jena.rdf.model.Property;
 import com.hp.hpl.jena.rdf.model.Resource;
 
 /**
- * Demonstrate how to work-around 3.3.1 limitation when it comes to
- * TAB-Traversal and checkbox editors. 3.4 will hopefully provide provide an
- * out-of-the-box fix see bug 198502
+ * Demonstrate how to work-around 3.3.1 limitation when it comes to TAB-Traversal and checkbox
+ * editors. 3.4 will hopefully provide provide an out-of-the-box fix see bug 198502
  * 
  * @author Tom Schindl <tom.schindl@bestsolution.at>
  * 
  */
 public class TreeEditorTest {
+	private TreeNode selTreeNode = null;
+
 	public TreeEditorTest(final Shell shell) {
-		final TreeViewer treeViewer = new TreeViewer(shell, SWT.BORDER
-				| SWT.FULL_SELECTION);
+		final TreeViewer treeViewer = new TreeViewer(shell, SWT.BORDER | SWT.FULL_SELECTION);
 		treeViewer.getTree().setLinesVisible(true);
 		treeViewer.getTree().setHeaderVisible(true);
 
-		final TreeViewerFocusCellManager mgr = new TreeViewerFocusCellManager(
-				treeViewer, new FocusCellOwnerDrawHighlighter(treeViewer));
-		ColumnViewerEditorActivationStrategy actSupport = new ColumnViewerEditorActivationStrategy(
-				treeViewer) {
-			protected boolean isEditorActivationEvent(
-					ColumnViewerEditorActivationEvent event) {
-				return event.eventType == ColumnViewerEditorActivationEvent.TRAVERSAL
-						|| event.eventType == ColumnViewerEditorActivationEvent.MOUSE_DOUBLE_CLICK_SELECTION
+		final TreeViewerFocusCellManager mgr = new TreeViewerFocusCellManager(treeViewer, new FocusCellOwnerDrawHighlighter(treeViewer));
+		ColumnViewerEditorActivationStrategy actSupport = new ColumnViewerEditorActivationStrategy(treeViewer) {
+			protected boolean isEditorActivationEvent(ColumnViewerEditorActivationEvent event) {
+				return event.eventType == ColumnViewerEditorActivationEvent.TRAVERSAL || event.eventType == ColumnViewerEditorActivationEvent.MOUSE_DOUBLE_CLICK_SELECTION
 						|| (event.eventType == ColumnViewerEditorActivationEvent.KEY_PRESSED && (event.keyCode == SWT.CR || event.character == ' '))
 						|| event.eventType == ColumnViewerEditorActivationEvent.PROGRAMMATIC;
 			}
 		};
 
-		TreeViewerEditor.create(treeViewer, mgr, actSupport,
-				ColumnViewerEditor.TABBING_HORIZONTAL
-						| ColumnViewerEditor.TABBING_MOVE_TO_ROW_NEIGHBOR
-						| ColumnViewerEditor.TABBING_VERTICAL
-						| ColumnViewerEditor.KEYBOARD_ACTIVATION);
+		TreeViewerEditor.create(treeViewer, mgr, actSupport, ColumnViewerEditor.TABBING_HORIZONTAL | ColumnViewerEditor.TABBING_MOVE_TO_ROW_NEIGHBOR
+				| ColumnViewerEditor.TABBING_VERTICAL | ColumnViewerEditor.KEYBOARD_ACTIVATION);
 
-		final TextCellEditor textCellEditor = new TextCellEditor(
-				treeViewer.getTree());
-		final CheckboxCellEditor checkboxCellEditor = new CheckboxCellEditor(
-				treeViewer.getTree());
-
+		final TextCellEditor textCellEditor = new TextCellEditor(treeViewer.getTree());
+		final CheckboxCellEditor checkboxCellEditor = new CheckboxCellEditor(treeViewer.getTree());
+		// ===================================================================================================
 		TreeViewerColumn column = new TreeViewerColumn(treeViewer, SWT.NONE);
 		column.getColumn().setWidth(200);
 		column.getColumn().setMoveable(true);
@@ -125,11 +121,12 @@ public class TreeEditorTest {
 			}
 
 			protected void setValue(Object treeNode, Object value) {
-				((TreeNode) treeNode).sourceRowNum = Integer.parseInt(value
-						.toString());
+				((TreeNode) treeNode).sourceRowNum = Integer.parseInt(value.toString());
 				treeViewer.update(treeNode, null);
 			}
 		});
+
+		// ===================================================================================================
 
 		column = new TreeViewerColumn(treeViewer, SWT.NONE);
 		column.getColumn().setWidth(200);
@@ -156,11 +153,11 @@ public class TreeEditorTest {
 			}
 
 			protected void setValue(Object treeNode, Object value) {
-				((TreeNode) treeNode).sourceRowNum = Integer.parseInt(value
-						.toString());
+				((TreeNode) treeNode).sourceRowNum = Integer.parseInt(value.toString());
 				treeViewer.update(treeNode, null);
 			}
 		});
+		// ===================================================================================================
 
 		column = new TreeViewerColumn(treeViewer, SWT.NONE);
 		column.getColumn().setWidth(200);
@@ -177,27 +174,27 @@ public class TreeEditorTest {
 			@Override
 			public void update(ViewerCell cell) {
 				super.update(cell);
-				
-				System.out.println("cell update --------------------");
-				ViewerRow viewerRow= cell.getViewerRow();
+
+				// System.out.println("cell update --------------------");
+				ViewerRow viewerRow = cell.getViewerRow();
 				;
-				System.out.println("((TreeNode)viewerRow.getElement()).treeRow = "+((TreeNode)viewerRow.getElement()).treeRow);
-				System.out.println("((TreeNode)viewerRow.getElement()).treeSubRow = "+((TreeNode)viewerRow.getElement()).treeSubRow);
-				System.out.println("viewerRow.toString()="+viewerRow.toString());
-				System.out.println("cell.getVisualIndex()="+cell.getVisualIndex());
-				System.out.println("cell.getBounds()="+cell.getBounds().toString());
-				System.out.println("viewerRow.getBounds()="+viewerRow.getBounds().toString());
-				System.out.println("viewerRow.getItem()="+viewerRow.getItem());
+				// System.out.println("((TreeNode)viewerRow.getElement()).treeRow = "+((TreeNode)viewerRow.getElement()).treeRow);
+				// System.out.println("((TreeNode)viewerRow.getElement()).treeSubRow = "+((TreeNode)viewerRow.getElement()).treeSubRow);
+				// System.out.println("viewerRow.toString()="+viewerRow.toString());
+				// System.out.println("cell.getVisualIndex()="+cell.getVisualIndex());
+				// System.out.println("cell.getBounds()="+cell.getBounds().toString());
+				// System.out.println("viewerRow.getBounds()="+viewerRow.getBounds().toString());
+				// System.out.println("viewerRow.getItem()="+viewerRow.getItem());
 
 				Device device = Display.getCurrent();
 				Color red = new Color(device, 255, 0, 0);
 				Color green = new Color(device, 0, 255, 0);
 				Color blue = new Color(device, 0, 0, 255);
-				if(currentColor == null){
+				if (currentColor == null) {
 					currentColor = blue;
 				}
-				System.out.println("currentColor="+currentColor);
-				
+				// System.out.println("currentColor="+currentColor);
+
 				if (currentColor.equals(red)) {
 					System.out.println("setting color to green");
 					cell.setBackground(green);
@@ -211,7 +208,7 @@ public class TreeEditorTest {
 					System.out.println("setting color to red");
 					cell.setBackground(red);
 					currentColor = red;
-				} else{
+				} else {
 					System.out.println("setting color to blue");
 					cell.setBackground(blue);
 					currentColor = blue;
@@ -219,34 +216,38 @@ public class TreeEditorTest {
 				treeViewer.getTree().deselectAll();
 			}
 
-
 		});
 		column.getColumn().addSelectionListener(new SelectionListener() {
 
 			@Override
 			public void widgetSelected(SelectionEvent e) {
-				System.out.println("COLUMN widgetSelected e.toString()="
-						+ e.toString());
+				System.out.println("COLUMN widgetSelected e.toString()=" + e.toString());
 			}
 
 			@Override
 			public void widgetDefaultSelected(SelectionEvent e) {
-				System.out.println("COLUMN widgetDefaultSelected e.toString()="
-						+ e.toString());
+				System.out.println("COLUMN widgetDefaultSelected e.toString()=" + e.toString());
 			}
 		});
 		column.getColumn().addListener(SWT.MouseDown, new Listener() {
 
 			@Override
 			public void handleEvent(Event event) {
-				System.out.println("event=" + event);
+				// System.out.println("event=" + event);
+				// System.out.println("event.widget=" + event.widget);
+				// System.out.println("event.widget.getData()=" + event.widget.getData());
+				// System.out.println("event=" + event);
+				// System.out.println("event=" + event);
+				// System.out.println("event=" + event);
+				// System.out.println("event=" + event);
+				// System.out.println("event=" + event);
 
 			}
 		});
 		column.setEditingSupport(new EditingSupport(treeViewer) {
 
 			protected boolean canEdit(Object treeNode) {
-				return true;
+				return false;
 			}
 
 			protected CellEditor getCellEditor(Object treeNode) {
@@ -263,23 +264,28 @@ public class TreeEditorTest {
 			}
 		});
 
+		// ===================================================================================================
+
 		treeViewer.getTree().addSelectionListener(new SelectionListener() {
 
 			@Override
 			public void widgetSelected(SelectionEvent e) {
-				System.out.println("widgetSelected e.getSource().toString()="
-						+ e.getSource().toString());
-				System.out.println("widgetSelected e.item=" + e.item);
-				System.out.println("widgetSelected e.toString=" + e.toString());
-				
+				// System.out.println("widgetSelected e.getSource().toString()="
+				// + e.getSource().toString());
+				// System.out.println("widgetSelected e.item=" + e.item);
+				// System.out.println("((TreeNode)e.item.getData()).treeRow=" +
+				// ((TreeNode)e.item.getData()).treeRow);
+				selTreeNode = (TreeNode) e.item.getData();
+				// System.out.println("widgetSelected e.toString=" + e.toString());
+
 			}
 
 			@Override
 			public void widgetDefaultSelected(SelectionEvent e) {
-				System.out
-						.println("widgetDefaultSelected e.getSource().toString()="
-								+ e.getSource().toString());
-				System.out.println("widgetDefaultSelected e.item=" + e.item);
+				// System.out
+				// .println("widgetDefaultSelected e.getSource().toString()="
+				// + e.getSource().toString());
+				// System.out.println("widgetDefaultSelected e.item=" + e.item);
 
 			}
 		});
@@ -287,21 +293,38 @@ public class TreeEditorTest {
 
 			@Override
 			public void mouseUp(MouseEvent e) {
-				System.out.println("mouse up");
-				System.out.println("e.getSource().toString()="
-						+ e.getSource().toString());
-				System.out.println("e.data=" + e.data);
-
+				// System.out.println("mouse up");
+				// System.out.println("e.getSource().toString()="
+				// + e.getSource().toString());
 			}
 
 			@Override
 			public void mouseDown(MouseEvent e) {
-				// TODO Auto-generated method stub
-
+				System.out.println("MouseDown on selTreeNode.treeRow: " + selTreeNode.treeRow);
 			}
 
 			@Override
 			public void mouseDoubleClick(MouseEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+		});
+		treeViewer.getTree().addKeyListener(new KeyListener() {
+
+			@Override
+			public void keyReleased(KeyEvent e) {
+				System.out.println("e.character = " + e.character);
+				System.out.println("e.character = " + e.character);
+				colorMatchedRows(treeViewer);
+
+				System.out.println("e.toString() = " + e.toString());
+				if (e.toString().equals("s")) {
+					System.out.println("selTreeNode.treeSubRow = " + selTreeNode.treeSubRow);
+				}
+			}
+
+			@Override
+			public void keyPressed(KeyEvent e) {
 				// TODO Auto-generated method stub
 
 			}
@@ -311,23 +334,14 @@ public class TreeEditorTest {
 		treeViewer.getControl().addTraverseListener(new TraverseListener() {
 
 			public void keyTraversed(TraverseEvent e) {
-				if ((e.detail == SWT.TRAVERSE_TAB_NEXT || e.detail == SWT.TRAVERSE_TAB_PREVIOUS)
-						&& mgr.getFocusCell().getColumnIndex() == 2) {
-					ColumnViewerEditor editor = treeViewer
-							.getColumnViewerEditor();
+				if ((e.detail == SWT.TRAVERSE_TAB_NEXT || e.detail == SWT.TRAVERSE_TAB_PREVIOUS) && mgr.getFocusCell().getColumnIndex() == 2) {
+					ColumnViewerEditor editor = treeViewer.getColumnViewerEditor();
 					ViewerCell cell = mgr.getFocusCell();
 
 					try {
-						Method m = ColumnViewerEditor.class.getDeclaredMethod(
-								"processTraverseEvent", new Class[] {
-										int.class, ViewerRow.class,
-										TraverseEvent.class });
+						Method m = ColumnViewerEditor.class.getDeclaredMethod("processTraverseEvent", new Class[] { int.class, ViewerRow.class, TraverseEvent.class });
 						m.setAccessible(true);
-						m.invoke(
-								editor,
-								new Object[] {
-										new Integer(cell.getColumnIndex()),
-										cell.getViewerRow(), e });
+						m.invoke(editor, new Object[] { new Integer(cell.getColumnIndex()), cell.getViewerRow(), e });
 					} catch (SecurityException e1) {
 						// TODO Auto-generated catch block
 						e1.printStackTrace();
@@ -362,7 +376,6 @@ public class TreeEditorTest {
 		// root.colAssocResource.add((Resource) ECO.Substance);
 		// root.colAssocProperty.add((Resource) ECO.hasSubstance);
 		// root.colAssocResource.add((Resource) ECO.);
-		// root.colAssocProperty.add((Resource) ECO.casNumber);
 
 		root.sourceRowNum = 0;
 
@@ -374,8 +387,8 @@ public class TreeEditorTest {
 			rowNames.add("Benzene " + i);
 			rowNames.add("102-32-" + i);
 			row = new TreeNode(root, rowNames);
-			row.treeRow=i;
-			row.treeSubRow=0;
+			row.treeRow = i;
+			row.treeSubRow = 0;
 			row.sourceRowNum = i;
 			root.child.add(row);
 
@@ -383,8 +396,8 @@ public class TreeEditorTest {
 			subRow.colLabels.add("TRACI");
 			subRow.colLabels.add("Benzene x" + i);
 			subRow.colLabels.add("102-32-" + i);
-			subRow.treeRow=i;
-			subRow.treeSubRow=1;
+			subRow.treeRow = i;
+			subRow.treeSubRow = 1;
 
 			subRow.sourceRowNum = i * 10 + 1;
 			row.child.add(subRow);
@@ -393,12 +406,60 @@ public class TreeEditorTest {
 			subRow.colLabels.add("ReCiPe");
 			subRow.colLabels.add("Benzene " + i);
 			subRow.colLabels.add("102-32-" + i);
-			subRow.treeRow=i;
-			subRow.treeSubRow=2;
+			subRow.treeRow = i;
+			subRow.treeSubRow = 2;
 			subRow.sourceRowNum = i * 10 + 2;
 			row.child.add(subRow);
 		}
 		return root;
+	}
+
+	private void colorMatchedRows(TreeViewer treeViewer) {
+		Device device = Display.getCurrent();
+		Color red = new Color(device, 255, 0, 0);
+		Color green = new Color(device, 0, 255, 0);
+		Color blue = new Color(device, 0, 0, 255);
+		Color pink = new Color(device, 255, 150, 150);
+		TreeNode trunk = (TreeNode) treeViewer.getInput();
+		// for (TreeNode row : trunk.child) {
+		for (int i = 0; i < trunk.child.size(); i++) {
+			TreeNode row = trunk.child.get(i);
+			TreeItem treeItem = treeViewer.getTree().getItem(i);
+
+			for (int j = 0; j < row.child.size(); j++) {
+				TreeNode subRow = row.child.get(j);
+				for (int k = 0; k < subRow.colLabels.size(); k++) {
+					String queryColLabel = row.colLabels.get(k);
+					String refColLabel = subRow.colLabels.get(k);
+					System.out.println("queryColLabel = "+queryColLabel);
+					System.out.println("refColLabel = "+refColLabel);
+					if (refColLabel.equals(queryColLabel)) {
+//						int itemCount = treeItem.getItemCount();
+//						System.out.println("at row: " + i + ", subRow: " + j + ", col: " + k + ", itemCount = " + itemCount);
+//						treeItem.setBackground(blue);
+//						TreeItem thing = treeItem.getItem(0);
+//						thing.setBackground(pink);
+//						System.out.println("at row: " + i + ", subRow: " + j + ", col: " + k + ", here's the treeItem.getItem(0): " + treeItem.getItem(0));
+//						System.out.println("at row: " + i + ", subRow: " + j + ", col: " + k + ", here's the treeItem.getItem(0).getData(): " + thing.getData());
+
+						 TreeColumn treeColumnX = treeViewer.getTree().getColumn(k);
+//						 Display thing = treeColumnX.getDisplay();
+//						  Object thing = treeColumnX.getData();
+
+						 System.out.println("treeColumnX.getText() = "+treeColumnX.getText());
+						// TreeItem[] thing = treeColumnX.getParent().getItems();
+						// Object thing = treeItem.getData();
+						// System.out.println("thing: " + thing);
+
+					}
+				}
+			}
+		}
+
+		for (TreeColumn column : treeViewer.getTree().getColumns()) {
+			System.out.println("column.getData = " + column.getData());
+
+		}
 	}
 
 	public static void main(String[] args) {
@@ -457,7 +518,6 @@ public class TreeEditorTest {
 		public List<String> colLabels = new ArrayList<String>();
 		public int treeRow;
 		public int treeSubRow;
-
 
 		public int sourceRowNum;
 
