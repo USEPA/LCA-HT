757cbddda2b12fe9ec6e22bc06dd8eef948ab913
Tom Transue <Tom@Lynn-PC>
2014-05-02 08:27:10 -0400

    - Clean-up a little, including Windows log file name compatibility

--- HarmonizationTool/src/org/apache/log4j/LoggerManager.java
+++ HarmonizationTool/src/org/apache/log4j/LoggerManager.java
@@ -6,6 +6,8 @@ import java.io.IOException;
 import java.util.Date;
 
 public class LoggerManager {
+	private static String timestampValidFmt = Util.getPreferenceStore().getString("startTimestamp").replace(":", "-")
+			.substring(0, Util.getPreferenceStore().getString("startTimestamp").length() - 5);
 
 	public static void Init() {
 		System.out.println("The logger init is executing");
@@ -17,12 +19,7 @@ public class LoggerManager {
 
 	private static void setUpRootLogger() {
 		Logger rootLogger = Logger.getRootLogger();
-		// LoggerManager.setLogFile(Util.getPreferenceStore().getString(
-		// "outputDirectory")
-		// + "/debug_" + Util.getPreferenceStore().getString("logFile"));
-
 		BasicConfigurator.configure();
-
 		rootLogger.setLevel(Level.DEBUG);
 
 		PatternLayout layout = new PatternLayout("[%t] %-5p %c %x - %m%n");
@@ -31,21 +28,15 @@ public class LoggerManager {
 		rootLogger.addAppender(new ConsoleAppender(layout));
 		try {
 			// Define file appender with layout and output log file name
-			RollingFileAppender fileAppender = new RollingFileAppender(layout,
-					Util.getPreferenceStore().getString("outputDirectory")
-							+ "/"
-							+ Util.getPreferenceStore().getString(
-									"outputFileRoot")
-							+ "_log_"
-							+ Util.getPreferenceStore().getString(
-									"startTimestamp") + ".txt");
+			RollingFileAppender fileAppender = new RollingFileAppender(layout, Util.getPreferenceStore().getString("outputDirectory") + "/"
+					+ Util.getPreferenceStore().getString("outputFileRoot") + "_debug_" + timestampValidFmt + ".txt");
 			// Add the appender to root logger
 			rootLogger.addAppender(fileAppender);
 		} catch (IOException e) {
 			System.out.println("Failed to add appender !!");
+			System.out.println("e =" + e);
 		}
-		rootLogger
-				.info("Started LCAHT at: " + Util.getLocalDateFmt(new Date()));
+		rootLogger.info("Started LCAHT at: " + Util.getLocalDateFmt(new Date()));
 	}
 
 	private static void setUpScriptLogger() {
@@ -54,39 +45,16 @@ public class LoggerManager {
 		PatternLayout layout = new PatternLayout("%m%n");
 		try {
 			// Define file appender with layout and output log file name
-			RollingFileAppender fileAppender = new RollingFileAppender(layout,
-					Util.getPreferenceStore().getString("outputDirectory")
-							+ "/"
-							+ Util.getPreferenceStore().getString(
-									"outputFileRoot")
-							+ "_script_"
-							+ Util.getPreferenceStore().getString(
-									"startTimestamp") + ".txt");
+			RollingFileAppender fileAppender = new RollingFileAppender(layout, Util.getPreferenceStore().getString("outputDirectory") + "/"
+					+ Util.getPreferenceStore().getString("outputFileRoot") + "_script_" + timestampValidFmt + ".txt");
 
 			// Add the appender to root logger
 			scriptLogger.addAppender(fileAppender);
 		} catch (IOException e) {
 			System.out.println("Failed to add appender !!");
+			System.out.println("e =" + e);
 		}
 		scriptLogger.setLevel(Level.INFO);
-		scriptLogger.info("# Started LCAHT at: "
-				+ Util.getLocalDateFmt(new Date()));
+		scriptLogger.info("# Started LCAHT at: " + Util.getLocalDateFmt(new Date()));
 	}
-
-	// public static void setLogFile(String filename) {
-	// Logger rootLogger = Logger.getRootLogger();
-	//
-	// PatternLayout layout = new PatternLayout(
-	// "%d{ISO8601} [%t] %-5p %c %x - %m%n");
-	// rootLogger.addAppender(new ConsoleAppender(layout));
-	// try {
-	// // Define file appender with layout and output log file name
-	// RollingFileAppender fileAppender = new RollingFileAppender(layout,
-	// filename);
-	// // Add the appender to root logger
-	// rootLogger.addAppender(fileAppender);
-	// } catch (IOException e) {
-	// System.out.println("Failed to add appender !!");
-	// }
-	// }
 }
