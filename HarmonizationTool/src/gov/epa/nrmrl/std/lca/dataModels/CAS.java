d6ce81ab737915b9252e0e7e6734e2b164148f3e
Tom Transue <transue.tom@epa.gov>
2014-05-16 15:11:09 -0400

    - Added Flowable and CAS classes, more to come

---
+++ HarmonizationTool/src/gov/epa/nrmrl/std/lca/dataModels/CAS.java
@@ -0,0 +1,72 @@
+package gov.epa.nrmrl.std.lca.dataModels;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class CAS {
+	private static String cas;
+	private static Pattern acceptibleFormat = Pattern.compile("^\\d{2,}-\\d\\d-\\d$|^\\d{5,}$");
+
+	public static boolean validStandardFormat(String candidate) {
+		Matcher matcher = acceptibleFormat.matcher(candidate);
+		if (matcher.find()) {
+			return true;
+		}
+		return false;
+	}
+
+	public static String standardize(String candidate) {
+		if (validStandardFormat(candidate)) {
+			String standarizedCas = "";
+			while (candidate.startsWith("0")) {
+				candidate = candidate.substring(1);
+			}
+			Matcher digitMatcher = Pattern.compile("\\d").matcher(candidate);
+			int digitsCount = digitMatcher.groupCount();
+			for (int i = 0; i < digitsCount; i++) {
+				if (i == digitsCount - 3 || i == digitsCount - 1) {
+					standarizedCas += "-";
+				}
+				standarizedCas += digitMatcher.group(i);
+			}
+			return standarizedCas;
+		}
+		return null;
+	}
+	
+	public static String strip(String candidate) {
+		if (validStandardFormat(candidate)) {
+			String strippedCas = "";
+			while (candidate.startsWith("0")) {
+				candidate = candidate.substring(1);
+			}
+			Matcher digitMatcher = Pattern.compile("\\d").matcher(candidate);
+			int digitsCount = digitMatcher.groupCount();
+			for (int i = 0; i < digitsCount; i++) {
+//				if (i == digitsCount - 3 || i == digitsCount - 1) {
+//					standarizedCas += "-";
+//				}
+				strippedCas += digitMatcher.group(i);
+			}
+			return strippedCas;
+		}
+		return null;
+	}
+
+	public static boolean correctCheckSum(String candidate) {
+		String strippedCas = strip(candidate);
+		if (strippedCas == null){
+			return false;
+		}
+		int multiplier=0;
+		int checksum=-Integer.parseInt(strippedCas.substring(strippedCas.length(),1));
+		for (int i = strippedCas.length()-2;i>=0;i--){
+			multiplier++;
+			checksum+=multiplier*Integer.parseInt(strippedCas.substring(i,1));
+		}
+		if (checksum%10 == 0){
+			return true;
+		}
+		return false;
+	}
+}
---
+++ HarmonizationTool/src/gov/epa/nrmrl/std/lca/dataModels/Flowable.java
@@ -0,0 +1,9 @@
+package gov.epa.nrmrl.std.lca.dataModels;
+
+public class Flowable {
+	private static boolean relevantResource;
+	private static boolean relevantEmission;
+	private static String name;
+	private static CAS cas;
+	
+}
