274a8233e6309533c800de2321ac4f3f683e63b4
Tom Transue <transue.tom@epa.gov>
2014-07-15 15:58:25 -0400

    - Added getJavaClassFromRDFDatatype and getRDFDatatypeFromJavaClass to ActiveTDB (again)

--- HarmonizationTool/src/gov/epa/nrmrl/std/lca/ht/tdb/ActiveTDB.java
+++ HarmonizationTool/src/gov/epa/nrmrl/std/lca/ht/tdb/ActiveTDB.java
@@ -1,6 +1,8 @@
 package gov.epa.nrmrl.std.lca.ht.tdb;
 
 import java.io.File;
+import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -14,6 +16,7 @@ import harmonizationtool.model.FileMDKeeper;
 import harmonizationtool.model.PersonKeeper;
 import harmonizationtool.query.QGetAllProperties;
 import harmonizationtool.utils.Util;
+
 import org.eclipse.core.commands.Command;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
@@ -28,8 +31,10 @@ import org.eclipse.ui.commands.ICommandService;
 import org.eclipse.ui.services.IServiceLocator;
 
 import com.hp.hpl.jena.datatypes.RDFDatatype;
+import com.hp.hpl.jena.datatypes.xsd.XSDDatatype;
 import com.hp.hpl.jena.query.Dataset;
 import com.hp.hpl.jena.query.QuerySolution;
+import com.hp.hpl.jena.query.ReadWrite;
 import com.hp.hpl.jena.query.ResultSet;
 import com.hp.hpl.jena.rdf.model.Literal;
 import com.hp.hpl.jena.rdf.model.Model;
@@ -69,18 +74,178 @@ public class ActiveTDB implements IHandler, IActiveTDB {
 	}
 	
 	public static void replaceLiteral(Resource subject, Property predicate, RDFDatatype rdfDatatype, Object thingLiteral) {
-		Literal newRDFNode = model.createTypedLiteral(thingLiteral, rdfDatatype);
-		NodeIterator nodeIterator = model.listObjectsOfProperty(subject, predicate);
-		while (nodeIterator.hasNext()) {
-			RDFNode rdfNode = nodeIterator.next();
-			if (rdfNode.isLiteral()) {
-				if (rdfNode.asLiteral().getDatatype().equals(rdfDatatype)){
-				model.removeAll(subject, predicate, rdfNode);
+		TDBDataset.begin(ReadWrite.WRITE);
+		try {
+			Literal newRDFNode = model.createTypedLiteral(thingLiteral, rdfDatatype);
+			NodeIterator nodeIterator = model.listObjectsOfProperty(subject, predicate);
+			while (nodeIterator.hasNext()) {
+				RDFNode rdfNode = nodeIterator.next();
+				if (rdfNode.isLiteral()) {
+					if (rdfNode.asLiteral().getDatatype().equals(rdfDatatype)) {
+						model.removeAll(subject, predicate, rdfNode);
+					}
 				}
 			}
+			model.add(subject, predicate, newRDFNode);
+			TDBDataset.commit();
+		} finally {
+			TDBDataset.end();
 		}
-		model.add(subject, predicate, newRDFNode);
 	}
+
+	public static void replaceLiteral(Resource subject, Property predicate, Object thingLiteral) {
+		RDFDatatype rdfDatatype = getRDFDatatypeFromJavaClass(thingLiteral);
+		TDBDataset.begin(ReadWrite.WRITE);
+		try {
+			Literal newRDFNode = model.createTypedLiteral(thingLiteral, rdfDatatype);
+			NodeIterator nodeIterator = model.listObjectsOfProperty(subject, predicate);
+			while (nodeIterator.hasNext()) {
+				RDFNode rdfNode = nodeIterator.next();
+				if (rdfNode.isLiteral()) {
+					if (rdfNode.asLiteral().getDatatype().equals(rdfDatatype)) {
+						model.removeAll(subject, predicate, rdfNode);
+					}
+				}
+			}
+			model.add(subject, predicate, newRDFNode);
+			TDBDataset.commit();
+		} finally {
+			TDBDataset.end();
+		}
+	}
+
+	public static RDFDatatype getRDFDatatypeFromJavaClass(Object object) {
+		if (object instanceof Float) {
+			return XSDDatatype.XSDfloat;
+		}
+		if (object instanceof Double) {
+			return XSDDatatype.XSDdouble;
+		}
+		if (object instanceof Integer) {
+			return XSDDatatype.XSDint;
+		}
+		if (object instanceof Long) {
+			return XSDDatatype.XSDlong;
+		}
+		if (object instanceof Short) {
+			return XSDDatatype.XSDshort;
+		}
+		if (object instanceof Byte) {
+			return XSDDatatype.XSDbyte;
+		}
+		if (object instanceof BigInteger) {
+			return XSDDatatype.XSDinteger;
+		}
+		if (object instanceof BigDecimal) {
+			return XSDDatatype.XSDdecimal;
+		}
+		if (object instanceof Boolean) {
+			return XSDDatatype.XSDboolean;
+		}
+		if (object instanceof String) {
+			return XSDDatatype.XSDstring;
+		}
+		if (object instanceof Date) {
+			return XSDDatatype.XSDdateTime;
+		}
+		return null;
+	}
+
+		@SuppressWarnings("rawtypes")
+	public static Class getJavaClassFromRDFDatatype(RDFDatatype rdfDatatype) {
+		if (rdfDatatype.equals(XSDDatatype.XSDfloat)) {
+			try {
+				return Class.forName("Float");
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+		}
+		if (rdfDatatype.equals(XSDDatatype.XSDdouble)) {
+			try {
+				return Class.forName("Double");
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+		}
+		if (rdfDatatype.equals(XSDDatatype.XSDint)) {
+			try {
+				return Class.forName("Integer");
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+		}
+		if (rdfDatatype.equals(XSDDatatype.XSDlong)) {
+			try {
+				return Class.forName("Long");
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+		}
+		if (rdfDatatype.equals(XSDDatatype.XSDshort)) {
+			try {
+				return Class.forName("Short");
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+		}
+		if (rdfDatatype.equals(XSDDatatype.XSDbyte)) {
+			try {
+				return Class.forName("Byte");
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+		}
+		if (rdfDatatype.equals(XSDDatatype.XSDinteger)) {
+			try {
+				return Class.forName("BigInteger");
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+		}
+		if (rdfDatatype.equals(XSDDatatype.XSDdecimal)) {
+			try {
+				return Class.forName("BigDecimal");
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+		}
+		if (rdfDatatype.equals(XSDDatatype.XSDboolean)) {
+			try {
+				return Class.forName("Boolean");
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+		}
+		if (rdfDatatype.equals(XSDDatatype.XSDstring)) {
+			try {
+				return Class.forName("String");
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+		}
+		if (rdfDatatype.equals(XSDDatatype.XSDdateTime)) {
+			try {
+				return Class.forName("Date");
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+		}
+		return null;
+	}
+
+//	public static void replaceLiteral(Resource subject, Property predicate, RDFDatatype rdfDatatype, Object thingLiteral) {
+//		Literal newRDFNode = model.createTypedLiteral(thingLiteral, rdfDatatype);
+//		NodeIterator nodeIterator = model.listObjectsOfProperty(subject, predicate);
+//		while (nodeIterator.hasNext()) {
+//			RDFNode rdfNode = nodeIterator.next();
+//			if (rdfNode.isLiteral()) {
+//				if (rdfNode.asLiteral().getDatatype().equals(rdfDatatype)){
+//				model.removeAll(subject, predicate, rdfNode);
+//				}
+//			}
+//		}
+//		model.add(subject, predicate, newRDFNode);
+//	}
 
 	public static void replaceLiteral(Resource subject, Property predicate, String stringLiteral) {
 		NodeIterator nodeIterator = model.listObjectsOfProperty(subject, predicate);
