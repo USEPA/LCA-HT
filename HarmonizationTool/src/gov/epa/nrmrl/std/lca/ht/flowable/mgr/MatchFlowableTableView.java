914e6505bd54f8fba59e6139b10abdb61d198407
Tom Transue <transue.tom@epa.gov>
2014-11-10 15:58:24 -0500

    - Got display of MatchFlowableTableView working pretty well - Need to tweak keyboard function, also improve hit counts

--- HarmonizationTool/src/gov/epa/nrmrl/std/lca/ht/flowable/mgr/MatchFlowableTableView.java
+++ HarmonizationTool/src/gov/epa/nrmrl/std/lca/ht/flowable/mgr/MatchFlowableTableView.java
@@ -31,22 +31,19 @@ import org.eclipse.jface.viewers.TextCellEditor;
 import org.eclipse.jface.viewers.ViewerCell;
 import org.eclipse.jface.window.ToolTip;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.ModifyEvent;
-import org.eclipse.swt.events.ModifyListener;
-import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Point;
-import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.TableColumn;
 import org.eclipse.swt.widgets.TableItem;
 import org.eclipse.swt.widgets.Text;
-import org.eclipse.swt.widgets.Widget;
 import org.eclipse.ui.part.ViewPart;
+import org.eclipse.wb.swt.SWTResourceManager;
 
 import com.hp.hpl.jena.query.QuerySolution;
 import com.hp.hpl.jena.query.ResultSet;
@@ -74,7 +71,10 @@ public class MatchFlowableTableView extends ViewPart {
 	private static Flowable flowableToMatch;
 	private static int dataTableRowNum = -1;
 	private static int searchRow = 1;
+	private static boolean readyToEdit = true;
 	private static int maxSearchResults = 50;
+	public static Text editorText;
+
 	// private static FlowableTableRow[] flowableTableRows;
 	private static List<FlowableTableRow> flowableTableRows;
 
@@ -95,15 +95,25 @@ public class MatchFlowableTableView extends ViewPart {
 		System.out.println("hello, from sunny MatchFlowableTableView!");
 		initializeTableViewer(outerComposite);
 		initialize();
+
 	}
 
 	private static void initializeTableViewer(Composite composite) {
 
-		Composite innerComposite = new Composite(outerComposite, SWT.NONE);
+		Composite innerComposite = new Composite(outerComposite, SWT.V_SCROLL);
 		innerComposite.setLayout(new FillLayout(SWT.HORIZONTAL));
 		GridData gridData = new GridData(SWT.LEFT, SWT.CENTER, true, false, 1, 1);
 		gridData.heightHint = 20;
 		innerComposite.setLayoutData(gridData);
+
+		composite.addListener(SWT.Resize, new Listener() {
+			@Override
+			public void handleEvent(Event event) {
+				table.setSize(table.getParent().getSize());
+			}
+			// THIS IS NOT PERFECT
+			// WHEN THE WINDOW IS RESIZED SMALLER, THE TABLE OVER RUNS A LITTLE
+		});
 
 		Button acceptAdvance = new Button(innerComposite, SWT.NONE);
 		acceptAdvance.setText("Next");
@@ -127,6 +137,9 @@ public class MatchFlowableTableView extends ViewPart {
 
 					FlowableTableRow flowableTableRow = (FlowableTableRow) viewerCell.getElement();
 
+					if (!readyToEdit) {
+						return false;
+					}
 					rowNumSelected = flowableTableRow.getRowNumber();
 					colNumSelected = viewerCell.getColumnIndex();
 					if (rowNumSelected == 0) {
@@ -136,7 +149,7 @@ public class MatchFlowableTableView extends ViewPart {
 						findMatches();
 						return false;
 					}
-					if ((rowNumSelected == searchRow) && (colNumSelected < 7)) {
+					if ((rowNumSelected == searchRow) && (colNumSelected < 6)) {
 						table.deselect(rowNumSelected);
 						return false;
 					}
@@ -144,15 +157,27 @@ public class MatchFlowableTableView extends ViewPart {
 						table.deselect(rowNumSelected);
 						return false;
 					}
-					if ((rowNumSelected != searchRow) && (colNumSelected < 7)) {
+					if ((rowNumSelected != searchRow) && (colNumSelected < 6)) {
 						assignMatch();
 						table.deselect(rowNumSelected);
 						return false;
 					}
 					table.deselect(rowNumSelected);
+					System.out.println("table = " + table);
 					return true;
+				} else if (event.eventType == ColumnViewerEditorActivationEvent.KEY_PRESSED) {
+					System.out.println("======= KEY_PRESSED, now ========");
+				} else if (event.eventType == ColumnViewerEditorActivationEvent.MOUSE_DOUBLE_CLICK_SELECTION) {
+					System.out.println("======= MOUSE_DOUBLE_CLICK_SELECTION, now ========");
+				} else if (event.eventType == ColumnViewerEditorActivationEvent.PROGRAMMATIC) {
+					System.out.println("======= PROGRAMMATIC, now ========");
+				} else if (event.eventType == ColumnViewerEditorActivationEvent.TRAVERSAL) {
+					System.out.println("======= TRAVERSAL, now ========");
+				} else {
+					System.out.println("======= something else. event.eventType = " + event.eventType);
 				}
 				return false;
+
 			}
 		};
 		activationSupport.setEnableEditorActivationWithKeyboard(true);
@@ -177,8 +202,6 @@ public class MatchFlowableTableView extends ViewPart {
 		createColumns();
 		// initialize();
 		TableProvider tableProvider = TableKeeper.getTableProvider(CSVTableView.getTableProviderKey());
-
-		tableViewer.setInput(flowableTableRows);
 
 		DataRow dataRow = tableProvider.getData().get(rowNumber);
 		dataTableRowNum = rowNumber;
@@ -221,10 +244,27 @@ public class MatchFlowableTableView extends ViewPart {
 		flowableTableRow.getColumnValues().set(6, "Click to Search -->");
 		flowableTableRows.add(flowableTableRow);
 
-		tableViewer.setInput(flowableTableRows);
-		tableViewer.refresh();
-		updateMatchCounts();
+		// tableViewer.setInput(flowableTableRows);
+		// Table fred = table;
+		// table.getItemCount();
 		// showSearchResults(50);
+		tableViewer.setInput(flowableTableRows);
+
+		// Composite thing = table.getParent();
+		// thing.setRedraw(true);
+		// thing.redraw();
+		// Composite thing2 = thing.getParent();
+		// thing2.setRedraw(true);
+		// thing2.redraw();
+		// tableViewer.refresh();
+		Point p = table.getParent().getParent().getSize();
+		p.y -= 30;
+		table.setSize(p);
+		table.getItem(0).setBackground(SWTResourceManager.getColor(SWT.COLOR_YELLOW));
+		table.getItem(searchRow).setBackground(SWTResourceManager.getColor(SWT.COLOR_GRAY));
+		updateMatchCounts();
+
+		// tableViewer.refresh();
 	}
 
 	private static void removeColumns() {
@@ -252,7 +292,8 @@ public class MatchFlowableTableView extends ViewPart {
 				continue;
 			}
 		}
-		tableViewer.refresh();
+		updateMatchCounts();
+//		tableViewer.refresh();
 	}
 
 	private static void createColumns() {
@@ -295,25 +336,14 @@ public class MatchFlowableTableView extends ViewPart {
 			return;
 		}
 		LinkedHashMap<Resource, String> candidateMap = flowableToMatch.getMatchCandidates();
-		for (String value: candidateMap.values()){
+		for (String value : candidateMap.values()) {
 			int col = MatchStatus.getNumberBySymbol(value);
-			if (col>0 && col < 6){
+			if (col > 0 && col < 4) {
 				noMatch = false;
 			}
 			matchSummary[col]++;
 		}
-		
-//		for (int rowNum = 1; rowNum < table.getItemCount(); rowNum++) {
-//			TableItem tableItem = table.getItem(rowNum);
-//			for (int colNum = 0; colNum < 6; colNum++) {
-//				if (!tableItem.getText(colNum).equals("")) {
-//					if (colNum > 0 && colNum < 5) {
-//						noMatch = false;
-//					}
-//					matchSummary[colNum]++;
-//				}
-//			}
-//		}
+
 		TableItem tableItem = table.getItem(0);
 		for (int colNum = 0; colNum < 6; colNum++) {
 			tableItem.setText(colNum, matchSummary[colNum].toString());
@@ -321,16 +351,14 @@ public class MatchFlowableTableView extends ViewPart {
 
 		Util.findView(FlowsWorkflow.ID);
 		if (noMatch) {
-			// FlowsWorkflow.removeMatchFlowableRowNum(dataTableRowNum);
 			FlowsWorkflow.removeMatchFlowableRowNum(flowableToMatch.getFirstRow());
+			table.getItem(0).setBackground(SWTResourceManager.getColor(SWT.COLOR_YELLOW));
 
 		} else {
 			FlowsWorkflow.addMatchFlowableRowNum(flowableToMatch.getFirstRow());
-			// FlowsWorkflow.addMatchFlowableRowNum(dataTableRowNum);
+			table.getItem(0).setBackground(SWTResourceManager.getColor(SWT.COLOR_GREEN));
 		}
-		// tableViewer.refresh();
-		// Util.findView(CSVTableView.ID);
-		// CSVTableView.colorOneFlowableRow(dataTableRowNum);
+		tableViewer.refresh();
 	}
 
 	public static int getCsvTableRowNum() {
@@ -374,10 +402,14 @@ public class MatchFlowableTableView extends ViewPart {
 			String newString = MatchStatus.getByValue(colNumSelected).getSymbol();
 			tableItem.setText(colNumSelected, newString);
 			flowableTableRow.set(colNumSelected, newString);
+			if (colNumSelected > 0) {
+				candidateMap.put(flowableTabRow.getFlowable().getTdbResource(), newString);
+			} else {
+				candidateMap.remove(flowableTabRow.getFlowable().getTdbResource());
+			}
 			searchMap.put(flowableTabRow.getFlowable().getTdbResource(), newString);
-			candidateMap.put(flowableTabRow.getFlowable().getTdbResource(), newString);
 		}
-//		CSVTableView.colorOneFlowableRow(flowableToMatch.getFirstRow());
+		// CSVTableView.colorOneFlowableRow(flowableToMatch.getFirstRow());
 		updateMatchCounts();
 	}
 
@@ -532,67 +564,67 @@ public class MatchFlowableTableView extends ViewPart {
 		}
 	}
 
-//	private static class RowIndexColumnLabelProvider extends ColumnLabelProvider {
-//
-//		public RowIndexColumnLabelProvider() {
-//		}
-//
-//		@Override
-//		public String getToolTipText(Object element) {
-//			DataRow dataRow = null;
-//			try {
-//				dataRow = (DataRow) element;
-//			} catch (Exception e) {
-//				e.printStackTrace();
-//				System.out.println("element= " + element);
-//			}
-//			String t = "";
-//			try {
-//				t = dataRow.getRowToolTip();
-//
-//			} catch (Exception e) {
-//				System.out.println("dataRow=" + dataRow);
-//				e.printStackTrace();
-//			}
-//			return t;
-//		}
-//
-//		@Override
-//		public Point getToolTipShift(Object object) {
-//			return new Point(5, 5);
-//		}
-//
-//		@Override
-//		public int getToolTipDisplayDelayTime(Object object) {
-//			return 100; // msec
-//		}
-//
-//		@Override
-//		public int getToolTipTimeDisplayed(Object object) {
-//			return 5000; // msec
-//		}
-//
-//		@Override
-//		public String getText(Object element) {
-//			DataRow dataRow = null;
-//			try {
-//				dataRow = (DataRow) element;
-//			} catch (Exception e) {
-//				e.printStackTrace();
-//				System.out.println("element= " + element);
-//			}
-//			String s = "";
-//			try {
-//				// s = dataRow.getRowNumber() + 1 + "";
-//				int rowNumPlus1 = dataRow.getRowNumber() + 1;
-//				s = rowNumPlus1 + "";
-//			} catch (Exception e) {
-//				System.out.println("dataRow=" + dataRow);
-//				e.printStackTrace();
-//			}
-//			return s;
-//		}
-//	}
+	// private static class RowIndexColumnLabelProvider extends ColumnLabelProvider {
+	//
+	// public RowIndexColumnLabelProvider() {
+	// }
+	//
+	// @Override
+	// public String getToolTipText(Object element) {
+	// DataRow dataRow = null;
+	// try {
+	// dataRow = (DataRow) element;
+	// } catch (Exception e) {
+	// e.printStackTrace();
+	// System.out.println("element= " + element);
+	// }
+	// String t = "";
+	// try {
+	// t = dataRow.getRowToolTip();
+	//
+	// } catch (Exception e) {
+	// System.out.println("dataRow=" + dataRow);
+	// e.printStackTrace();
+	// }
+	// return t;
+	// }
+	//
+	// @Override
+	// public Point getToolTipShift(Object object) {
+	// return new Point(5, 5);
+	// }
+	//
+	// @Override
+	// public int getToolTipDisplayDelayTime(Object object) {
+	// return 100; // msec
+	// }
+	//
+	// @Override
+	// public int getToolTipTimeDisplayed(Object object) {
+	// return 5000; // msec
+	// }
+	//
+	// @Override
+	// public String getText(Object element) {
+	// DataRow dataRow = null;
+	// try {
+	// dataRow = (DataRow) element;
+	// } catch (Exception e) {
+	// e.printStackTrace();
+	// System.out.println("element= " + element);
+	// }
+	// String s = "";
+	// try {
+	// // s = dataRow.getRowNumber() + 1 + "";
+	// int rowNumPlus1 = dataRow.getRowNumber() + 1;
+	// s = rowNumPlus1 + "";
+	// } catch (Exception e) {
+	// System.out.println("dataRow=" + dataRow);
+	// e.printStackTrace();
+	// }
+	// return s;
+	// }
+	// }
 
 	private static TableViewerColumn createTableViewerColumn(String title, int bound, final int colNumber) {
 
@@ -628,30 +660,30 @@ public class MatchFlowableTableView extends ViewPart {
 		// tableViewerColumn.getColumn().addSelectionListener(nextSelectionListener);
 	}
 
-//	//
-//	private static SelectionListener colSelectionListener = new SelectionListener() {
-//
-//		private void doit(SelectionEvent e) {
-//			// System.out.println("SelectionListener event e= " + e);
-//			if (e.getSource() instanceof TableColumn) {
-//				// TableColumn col = (TableColumn) e.getSource();
-//				// colNumSelected = table.indexOf(col);
-//				// if (colNumSelected > 0) {
-//				// headerMenu.setVisible(true);
-//				// }
-//			}
-//		}
-//
-//		@Override
-//		public void widgetSelected(SelectionEvent e) {
-//			doit(e);
-//		};
-//
-//		@Override
-//		public void widgetDefaultSelected(SelectionEvent e) {
-//			doit(e);
-//		}
-//	};
+	// //
+	// private static SelectionListener colSelectionListener = new SelectionListener() {
+	//
+	// private void doit(SelectionEvent e) {
+	// // System.out.println("SelectionListener event e= " + e);
+	// if (e.getSource() instanceof TableColumn) {
+	// // TableColumn col = (TableColumn) e.getSource();
+	// // colNumSelected = table.indexOf(col);
+	// // if (colNumSelected > 0) {
+	// // headerMenu.setVisible(true);
+	// // }
+	// }
+	// }
+	//
+	// @Override
+	// public void widgetSelected(SelectionEvent e) {
+	// doit(e);
+	// };
+	//
+	// @Override
+	// public void widgetDefaultSelected(SelectionEvent e) {
+	// doit(e);
+	// }
+	// };
 
 	private static SelectionListener nextSelectionListener = new SelectionListener() {
 
@@ -731,6 +763,7 @@ public class MatchFlowableTableView extends ViewPart {
 		protected Object getValue(Object element) {
 			FlowableTableRow flowableTableRow = (FlowableTableRow) element;
 			System.out.println("== GET ==");
+			readyToEdit = false;
 			return flowableTableRow.get(colNumSelected);
 		}
 
@@ -743,6 +776,7 @@ public class MatchFlowableTableView extends ViewPart {
 				FlowableTableRow flowableTableRow = (FlowableTableRow) element;
 
 				System.out.println("== SET ==");
+				readyToEdit = true;
 				flowableTableRow.set(colNumSelected, (String) value);
 				table.deselectAll();
 				tableViewer.refresh();
@@ -754,6 +788,7 @@ public class MatchFlowableTableView extends ViewPart {
 	public static class TextCellEditorMod1 extends TextCellEditor {
 		public TextCellEditorMod1(Composite parent, KeyStroke keyStroke, char[] autoActivationCharacters) {
 			super(parent);
+			editorText = this.text;
 			// System.out.println("keyStroke = " + keyStroke);
 			// System.out.println("autoActivationCharacters" + autoActivationCharacters.toString());
 		}
