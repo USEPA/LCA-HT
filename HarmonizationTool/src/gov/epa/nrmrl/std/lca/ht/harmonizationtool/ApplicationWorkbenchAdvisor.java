e78123fee4ea6d86b978f0163338413d5440cf13
Tony Howard <tahoward@nc.rr.com>
2015-05-19 23:17:10 -0400

    Remove Maven and General pages

--- HarmonizationTool/src/gov/epa/nrmrl/std/lca/ht/harmonizationtool/ApplicationWorkbenchAdvisor.java
+++ HarmonizationTool/src/gov/epa/nrmrl/std/lca/ht/harmonizationtool/ApplicationWorkbenchAdvisor.java
@@ -1,6 +1,9 @@
 package gov.epa.nrmrl.std.lca.ht.harmonizationtool;
 
 import gov.epa.nrmrl.std.lca.ht.perspectives.LCIWorkflowPerspective;
+
+import org.eclipse.jface.preference.PreferenceManager;
+import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.application.IWorkbenchConfigurer;
 import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
 import org.eclipse.ui.application.WorkbenchAdvisor;
@@ -13,6 +16,12 @@ public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
 		return new ApplicationWorkbenchWindowAdvisor(configurer);
 	}
 
+	@Override
+	public void postStartup() {
+	    PreferenceManager pm = PlatformUI.getWorkbench().getPreferenceManager( );
+	    pm.remove( "org.eclipse.m2e.core.preferences.Maven2PreferencePage" );
+	    pm.remove( "org.eclipse.ui.preferencePages.Workbench" );
+	}
 	@Override
 	public void initialize(IWorkbenchConfigurer configurer) {
 		System.out.println("configurer.setSaveAndRestore(true)");
